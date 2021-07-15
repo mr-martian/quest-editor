716f5450fc9e76239c61b50c0c11edf762e5ed7b
killerbee <7351905+killerbee13@users.noreply.github.com>
2021-07-14 20:26:38 -0700

    Basic setup

---
+++ src/Core/core_main.cpp
@@ -0,0 +1,10 @@
+/* *****************************************************************************
+ * Quest Text Editor
+ * Copyright (c) 2021 Daniel Swanson (@mr-martian), d-us-vb,
+ *   killerbee (@killerbee13), Richard (@CodeTriangle)
+ *
+ * <Insert License Here>
+ *
+ * ****************************************************************************/
+
+int main(int argc, char** argv) {}
